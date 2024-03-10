1f9c1f0625be405b1277077e8f55e5cbb3f14261
Felipe Sanches <felinobre100@gmail.com>
2024-03-09 23:16:24 -0300

    get a file

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 # Uncomment this to pass the first stage
 import socket
 import threading
+import sys
 
 def main():
     print("Logs from your program will appear here!")
     while True:
         server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-        thr = threading.Thread(target= handle_connection, args=(server_socket,))
+        thr = threading.Thread(target= handle_connection, args=(server_socket, dir,))
         thr.start()
 
-def handle_connection(server_socket):
+def handle_connection(server_socket, dir):
     server_socket.listen(1)
     conn, addr = server_socket.accept() # wait for client
     data = conn.recv(1024).decode().split("\r\n")
@@ -21,6 +22,16 @@ def handle_connection(server_socket):
 
     if path == "/":
         conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    elif path.startswith("/files/"):
+        file_name = path.split("/files/")[1]
+        full_path = f"{dir}/{file_name}"
+        try:
+            with open(full_path, "rb") as file:
+                file_content = file.read()
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: octet-stream\r\nContent-Length: {len(file_content)}\r\n\r\n{file_content}"
+                conn.sendall(response.encode())
+        except FileNotFoundError:
+            conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
     elif path.startswith("/echo/"):
         msg = path.split("/echo/")[1]
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(msg)}\r\n\r\n{msg}"
@@ -36,4 +47,7 @@ def handle_connection(server_socket):
 
 
 if __name__ == "__main__":
-    main()
+    dir = ""
+    if len(sys.argv) >= 3:
+        dir = sys.argv[2]
+    main(dir)
